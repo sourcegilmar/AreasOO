bbaec4b5661758d4d78dfc3ecb2e02f430757554
sourcegilmar <gilmardw@gmail.com>
2021-03-28 10:55:28 -0300

    1a refatoracao - Implem. classes entidades

--- src/entidades/Quadrado.java
+++ src/entidades/Quadrado.java
@@ -1,5 +1,17 @@
 package entidades;
 
 public class Quadrado {
+	double lado;
 
+	public double getLado() {
+		return lado;
+	}
+
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+
+	public double area() {
+		return Math.pow(lado, 2);
+	}
 }
--- src/entidades/Triangulo.java
+++ src/entidades/Triangulo.java
@@ -1,5 +1,26 @@
 package entidades;
 
 public class Triangulo {
+	private double base;
+	private double altura;
+	
+	public double getBase() {
+		return base;
+	}
 
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public double area() {
+		return (this.base * this.altura) / 2;
+	}
 }
